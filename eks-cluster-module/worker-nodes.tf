457ab39cdd8bcfcb59a95ea7c3abfdc198bce300
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-06 09:11:29 -0600

    added neccessary policies to node role

--- eks-cluster-module/worker-nodes.tf
+++ eks-cluster-module/worker-nodes.tf
@@ -35,6 +35,16 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_csi_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
 resource "aws_iam_instance_profile" "eks_worker_profile" {
   name = "eks-worker-instance-profile"
   role = aws_iam_role.eks_worker_role.name
