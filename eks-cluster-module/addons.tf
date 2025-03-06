8cbf6377dbdf8247fbd46ba4ef7eb21a5b69efe8
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-06 09:13:02 -0600

    adding coredns and csi driver

--- eks-cluster-module/addons.tf
+++ eks-cluster-module/addons.tf
@@ -7,14 +7,14 @@ resource "aws_eks_addon" "vpc_cni" {
   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
 }
 
-# resource "aws_eks_addon" "coredns" {
-#   cluster_name                = aws_eks_cluster.eks_cluster.name
-#   addon_name                  = "coredns"
-#   addon_version               = "v1.11.4-eksbuild.2"
-#   resolve_conflicts_on_update = "PRESERVE"
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = aws_eks_cluster.eks_cluster.name
+  addon_name                  = "coredns"
+  addon_version               = "v1.11.4-eksbuild.2"
+  resolve_conflicts_on_update = "PRESERVE"
 
-#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-# }
+  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+}
 
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name                = aws_eks_cluster.eks_cluster.name
@@ -25,11 +25,11 @@ resource "aws_eks_addon" "kube_proxy" {
   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
 }
 
-# resource "aws_eks_addon" "ebs_csi" {
-#   cluster_name                = aws_eks_cluster.eks_cluster.name
-#   addon_name                  = "aws-ebs-csi-driver"
-#   addon_version               = "v1.39.0-eksbuild.1"
-#   resolve_conflicts_on_update = "PRESERVE"
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name                = aws_eks_cluster.eks_cluster.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.39.0-eksbuild.1"
+  resolve_conflicts_on_update = "PRESERVE"
 
-#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-# }
+  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+}
