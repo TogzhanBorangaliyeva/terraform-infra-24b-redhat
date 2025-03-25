d8582aadf8f6f1ea19e1df8bba918b57021f507a
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-25 13:32:33 -0500

    creating vpc

--- roots/devops-project-main/dev.tfvars
+++ roots/devops-project-main/dev.tfvars
@@ -15,25 +15,25 @@ private_subnets = [
 ]
 availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-# EKS tfvars
-cluster_name                  = "redhat-eks-cluster-dev"
-kubernetes_version            = "1.31"
-max_session_duration_cluster  = 43200
-eks_worker_node               = "redhat-eks-worker-node"
-instance_type                 = "t3.small"
-desired_capacity              = 2
-max_size                      = 4
-min_size                      = 1
-on_demand_base_capacity       = 0
-on_demand_percentage          = 20
-spot_max_price                = "0.0464"
-key_name                      = "eks-key"
-github_actions_terraform_role = "GitHubActionsTerraformIAMrole"
+# # EKS tfvars
+# cluster_name                  = "redhat-eks-cluster-dev"
+# kubernetes_version            = "1.31"
+# max_session_duration_cluster  = 43200
+# eks_worker_node               = "redhat-eks-worker-node"
+# instance_type                 = "t3.small"
+# desired_capacity              = 2
+# max_size                      = 4
+# min_size                      = 1
+# on_demand_base_capacity       = 0
+# on_demand_percentage          = 20
+# spot_max_price                = "0.0464"
+# key_name                      = "eks-key"
+# github_actions_terraform_role = "GitHubActionsTerraformIAMrole"
 
-# Autoscaler
-asg_name                           = "redhat-eks-cluster-dev-nodes"
-role_name                          = "cluster_autoscaler"
-policy_name                        = "cluster_autoscaler_policy"
-aws_oidc_provider_arn              = "arn:aws:iam::539247466139:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/D0BA4F2D549CA4763A7A95A488A22E8E"
-cluster_autoscaler_service_account = "cluster-autoscaler"
-# eks_cluster_arn = "arn:aws:eks:us-east-1:340924313311:cluster/temporary-eks-cluster-dev"
+# # Autoscaler
+# asg_name                           = "redhat-eks-cluster-dev-nodes"
+# role_name                          = "cluster_autoscaler"
+# policy_name                        = "cluster_autoscaler_policy"
+# aws_oidc_provider_arn              = "arn:aws:iam::539247466139:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/D0BA4F2D549CA4763A7A95A488A22E8E"
+# cluster_autoscaler_service_account = "cluster-autoscaler"
+# # eks_cluster_arn = "arn:aws:eks:us-east-1:340924313311:cluster/temporary-eks-cluster-dev"
--- roots/devops-project-main/variables.tf
+++ roots/devops-project-main/variables.tf
@@ -29,100 +29,100 @@ variable "private_subnets" {
 }
 
 
-# EKS variables
-variable "cluster_name" {
-  description = "EKS Cluster Name"
-  type        = string
-}
-
-variable "kubernetes_version" {
-  description = "Kubernetes version one release prior to the latest supported by EKS"
-  type        = string
-}
-
-variable "max_session_duration_cluster" {
-  description = "Maximum session duration for cluster role"
-  type        = number
-}
-
-variable "eks_worker_node" {
-  description = "Name for the EKS worker nodes"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type for the worker nodes in the launch template"
-  type        = string
-}
-
-variable "desired_capacity" {
-  description = "The desired number of instances in the Auto Scaling Group for the worker nodes"
-  type        = number
-}
-
-variable "max_size" {
-  description = "The maximum number of instances the Auto Scaling Group can scale to"
-  type        = number
-}
-
-variable "min_size" {
-  description = "The minimum number of instances the Auto Scaling Group should maintain"
-  type        = number
-}
-
-variable "on_demand_base_capacity" {
-  description = "The minimum number of On-Demand instances in the Auto Scaling Group"
-  type        = number
-}
-
-variable "on_demand_percentage" {
-  description = "The percentage of instances above the base capacity that should be On-Demand"
-  type        = number
-}
-
-variable "spot_max_price" {
-  description = "The maximum price to pay per hour for Spot instances"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key for worker nodes"
-  type        = string
-}
-
-variable "github_actions_terraform_role" {
-  description = "GitHub Actions Terraform Role Name"
-  type        = string
-}
-
-#Autoscaler
-variable "asg_name" {
-  type = string
-}
-
-variable "role_name" {
-  description = "cluster-autoscaler"
-  type        = string
-}
-
-
-variable "policy_name" {
-  description = "cluster-autoscaler-policy"
-  type        = string
-}
-
-
-variable "aws_oidc_provider_arn" {
-  description = "The OIDC provider ARN for the EKS cluster"
-  type        = string
-}
-
-variable "cluster_autoscaler_service_account" {
-  description = "The service account name for Cluster Autoscaler"
-  type        = string
-}
-
-# variable "eks_cluster_arn" {
-#   description = "The ARN of the EKS cluster"
+# # EKS variables
+# variable "cluster_name" {
+#   description = "EKS Cluster Name"
 #   type        = string
 # }
+
+# variable "kubernetes_version" {
+#   description = "Kubernetes version one release prior to the latest supported by EKS"
+#   type        = string
+# }
+
+# variable "max_session_duration_cluster" {
+#   description = "Maximum session duration for cluster role"
+#   type        = number
+# }
+
+# variable "eks_worker_node" {
+#   description = "Name for the EKS worker nodes"
+#   type        = string
+# }
+
+# variable "instance_type" {
+#   description = "The EC2 instance type for the worker nodes in the launch template"
+#   type        = string
+# }
+
+# variable "desired_capacity" {
+#   description = "The desired number of instances in the Auto Scaling Group for the worker nodes"
+#   type        = number
+# }
+
+# variable "max_size" {
+#   description = "The maximum number of instances the Auto Scaling Group can scale to"
+#   type        = number
+# }
+
+# variable "min_size" {
+#   description = "The minimum number of instances the Auto Scaling Group should maintain"
+#   type        = number
+# }
+
+# variable "on_demand_base_capacity" {
+#   description = "The minimum number of On-Demand instances in the Auto Scaling Group"
+#   type        = number
+# }
+
+# variable "on_demand_percentage" {
+#   description = "The percentage of instances above the base capacity that should be On-Demand"
+#   type        = number
+# }
+
+# variable "spot_max_price" {
+#   description = "The maximum price to pay per hour for Spot instances"
+#   type        = string
+# }
+
+# variable "key_name" {
+#   description = "SSH key for worker nodes"
+#   type        = string
+# }
+
+# variable "github_actions_terraform_role" {
+#   description = "GitHub Actions Terraform Role Name"
+#   type        = string
+# }
+
+# #Autoscaler
+# variable "asg_name" {
+#   type = string
+# }
+
+# variable "role_name" {
+#   description = "cluster-autoscaler"
+#   type        = string
+# }
+
+
+# variable "policy_name" {
+#   description = "cluster-autoscaler-policy"
+#   type        = string
+# }
+
+
+# variable "aws_oidc_provider_arn" {
+#   description = "The OIDC provider ARN for the EKS cluster"
+#   type        = string
+# }
+
+# variable "cluster_autoscaler_service_account" {
+#   description = "The service account name for Cluster Autoscaler"
+#   type        = string
+# }
+
+# # variable "eks_cluster_arn" {
+# #   description = "The ARN of the EKS cluster"
+# #   type        = string
+# # }
