4a6d8df5857008602607e15ca7c19f5d6eb16278
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-25 15:33:56 -0500

    removed addons

--- eks-cluster-module/addons.tf
+++ eks-cluster-module/addons.tf
@@ -1,35 +1,35 @@
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name                = aws_eks_cluster.eks_cluster.name
-  addon_name                  = "vpc-cni"
-  addon_version               = "v1.19.2-eksbuild.1"
-  resolve_conflicts_on_update = "PRESERVE"
+# resource "aws_eks_addon" "vpc_cni" {
+#   cluster_name                = aws_eks_cluster.eks_cluster.name
+#   addon_name                  = "vpc-cni"
+#   addon_version               = "v1.19.2-eksbuild.1"
+#   resolve_conflicts_on_update = "PRESERVE"
 
-  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+# }
 
-resource "aws_eks_addon" "coredns" {
-  cluster_name                = aws_eks_cluster.eks_cluster.name
-  addon_name                  = "coredns"
-  addon_version               = "v1.11.4-eksbuild.2"
-  resolve_conflicts_on_update = "PRESERVE"
+# resource "aws_eks_addon" "coredns" {
+#   cluster_name                = aws_eks_cluster.eks_cluster.name
+#   addon_name                  = "coredns"
+#   addon_version               = "v1.11.4-eksbuild.2"
+#   resolve_conflicts_on_update = "PRESERVE"
 
-  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+# }
 
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name                = aws_eks_cluster.eks_cluster.name
-  addon_name                  = "kube-proxy"
-  addon_version               = "v1.31.3-eksbuild.2"
-  resolve_conflicts_on_update = "PRESERVE"
+# resource "aws_eks_addon" "kube_proxy" {
+#   cluster_name                = aws_eks_cluster.eks_cluster.name
+#   addon_name                  = "kube-proxy"
+#   addon_version               = "v1.31.3-eksbuild.2"
+#   resolve_conflicts_on_update = "PRESERVE"
 
-  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+# }
 
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name                = aws_eks_cluster.eks_cluster.name
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.39.0-eksbuild.1"
-  resolve_conflicts_on_update = "PRESERVE"
+# resource "aws_eks_addon" "ebs_csi" {
+#   cluster_name                = aws_eks_cluster.eks_cluster.name
+#   addon_name                  = "aws-ebs-csi-driver"
+#   addon_version               = "v1.39.0-eksbuild.1"
+#   resolve_conflicts_on_update = "PRESERVE"
 
-  depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster, aws_autoscaling_group.eks_asg]
+# }
