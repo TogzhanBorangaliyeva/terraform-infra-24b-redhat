58a5c0c2f1506d4b226d7cf2118500e89207ffec
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-25 14:46:23 -0500

    creating eks [][D3

--- roots/devops-project-main/dev.tfvars
+++ roots/devops-project-main/dev.tfvars
@@ -15,20 +15,20 @@ private_subnets = [
 ]
 availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-# # EKS tfvars
-# cluster_name                  = "redhat-eks-cluster-dev"
-# kubernetes_version            = "1.31"
-# max_session_duration_cluster  = 43200
-# eks_worker_node               = "redhat-eks-worker-node"
-# instance_type                 = "t3.small"
-# desired_capacity              = 2
-# max_size                      = 4
-# min_size                      = 1
-# on_demand_base_capacity       = 0
-# on_demand_percentage          = 20
-# spot_max_price                = "0.0464"
-# key_name                      = "eks-key"
-# github_actions_terraform_role = "platform-tools-24b-redhat-GHA-role"
+# EKS tfvars
+cluster_name                  = "redhat-eks-cluster-dev"
+kubernetes_version            = "1.31"
+max_session_duration_cluster  = 43200
+eks_worker_node               = "redhat-eks-worker-node"
+instance_type                 = "t3.small"
+desired_capacity              = 2
+max_size                      = 4
+min_size                      = 1
+on_demand_base_capacity       = 0
+on_demand_percentage          = 20
+spot_max_price                = "0.0464"
+key_name                      = "eks-key"
+github_actions_terraform_role = "platform-tools-24b-redhat-GHA-role"
 
 # # Autoscaler
 # asg_name                           = "redhat-eks-cluster-dev-nodes"
--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -16,71 +16,71 @@ module "vpc" {
   private_subnets    = var.private_subnets
 }
 
-# #EKS module
-# module "eks" {
-#   source                       = "../../eks-cluster-module"
-#   public_subnet_ids            = module.vpc.public_subnet_ids
-#   vpc_id                       = module.vpc.vpc_id
-#   cluster_name                 = var.cluster_name
-#   kubernetes_version           = var.kubernetes_version
-#   max_session_duration_cluster = var.max_session_duration_cluster
+#EKS module
+module "eks" {
+  source                       = "../../eks-cluster-module"
+  public_subnet_ids            = module.vpc.public_subnet_ids
+  vpc_id                       = module.vpc.vpc_id
+  cluster_name                 = var.cluster_name
+  kubernetes_version           = var.kubernetes_version
+  max_session_duration_cluster = var.max_session_duration_cluster
 
-#   eks_worker_node               = var.eks_worker_node
-#   instance_type                 = var.instance_type
-#   desired_capacity              = var.desired_capacity
-#   max_size                      = var.max_size
-#   min_size                      = var.min_size
-#   on_demand_base_capacity       = var.on_demand_base_capacity
-#   on_demand_percentage          = var.on_demand_percentage
-#   spot_max_price                = var.spot_max_price
-#   key_name                      = var.key_name
-#   github_actions_terraform_role = var.github_actions_terraform_role
-# }
+  eks_worker_node               = var.eks_worker_node
+  instance_type                 = var.instance_type
+  desired_capacity              = var.desired_capacity
+  max_size                      = var.max_size
+  min_size                      = var.min_size
+  on_demand_base_capacity       = var.on_demand_base_capacity
+  on_demand_percentage          = var.on_demand_percentage
+  spot_max_price                = var.spot_max_price
+  key_name                      = var.key_name
+  github_actions_terraform_role = var.github_actions_terraform_role
+}
 
-# data "aws_eks_cluster_auth" "eks_cluster_auth" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  name = module.eks.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = module.eks.eks_cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
-# }
+provider "kubernetes" {
+  host                   = module.eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# resource "kubernetes_config_map" "aws_auth" {
-#   metadata {
-#     name      = "aws-auth"
-#     namespace = "kube-system"
-#   }
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
 
-#   depends_on = [
-#     module.eks
-#   ]
+  depends_on = [
+    module.eks
+  ]
 
-#   data = {
-#     mapRoles = <<EOT
-# - rolearn: ${module.eks.eks_worker_role_arn}
-#   username: system:node:{{EC2PrivateDNSName}}
-#   groups:
-#     - system:bootstrappers
-#     - system:nodes
-#     - system:masters
-# - rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.github_actions_terraform_role}
-#   username: terraform
-#   groups:
-#     - system:masters
-# EOT
-#     # Add the IAM user `mike` here
-#     mapUsers = <<EOT
-# - userarn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/mike
-#   username: mike
-#   groups:
-#     - system:masters
-# EOT
-#   }
-# }
+  data = {
+    mapRoles = <<EOT
+- rolearn: ${module.eks.eks_worker_role_arn}
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+    - system:bootstrappers
+    - system:nodes
+    - system:masters
+- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.github_actions_terraform_role}
+  username: terraform
+  groups:
+    - system:masters
+EOT
+    # Add the IAM user `mike` here
+    mapUsers = <<EOT
+- userarn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/mike
+  username: mike
+  groups:
+    - system:masters
+EOT
+  }
+}
 
 # #Autoscaler
 # module "cluster_autoscaler" {
