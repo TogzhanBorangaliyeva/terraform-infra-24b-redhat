f065960566b1c045680428707994ce88b6023ef7
TogzhanBorangaliyeva <togzhan.bor@edu.312school.com>
2025-03-26 13:12:35 -0500

    karpenter module

--- roots/devops-project-main/variables.tf
+++ roots/devops-project-main/variables.tf
@@ -127,15 +127,15 @@ variable "github_actions_terraform_role" {
 # #   type        = string
 # # }
 
-# # karpenter
-# variable "eks_cluster_tls_cert_oidc" {
-#   type = string
-# }
+# karpenter
+variable "eks_cluster_tls_cert_oidc" {
+  type = string
+}
 
-# variable "oidc_thumbprint" {
-#   type = string
-# }
+variable "oidc_thumbprint" {
+  type = string
+}
 
-# variable "worker_node_iam_role" {
-#   type = string
-# }
+variable "worker_node_iam_role" {
+  type = string
+}
